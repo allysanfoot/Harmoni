a1e48dc55f51f5184b001a8951f6e5474e1c6836
allysanfoot <allysafoot@gmail.com>
2024-11-28 18:58:53 -0800

    HAPPY THANKSGIVING!

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage: React.FC = () => {
+    return (
+        <div>Login Page</div>
+    )
+}
+
+export default LoginPage
---
+++ client/src/pages/RegistrationPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RegistrationPage: React.FC = () => {
+    return (
+        <div>Registration Page</div>
+    )
+}
+
+export default RegistrationPage
