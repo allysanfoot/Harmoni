1c5f72999415843f78132ac24a45d4d1af582cc3
allysanfoot <allysafoot@gmail.com>
2024-11-28 17:55:26 -0800

    feat: adding jwt authorization

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -7,6 +7,8 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
+console.log("Database URL:", process.env.DATABASE_URL);
+
 pool.on("connect", () => {
     console.log("Connected to the database");
 });
